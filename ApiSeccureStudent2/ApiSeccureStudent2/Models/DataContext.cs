2a226ec01c3f335a35e8d3db0168169673e64c90
DanielAlexaderVasquezCazon <a2019115093@estudiantes.upsa.edu.bo>
2020-09-01 03:12:48 -0400

    Con controlador de student mas autorizacion

---
+++ ApiSeccureStudent2/ApiSeccureStudent2/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ApiSeccureStudent2.Models
+{
+    using System.Data.Entity;
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<ApiSeccureStudent2.Models.Student> Students { get; set; }
+    }
+}
---
+++ ApiSeccureStudent2/ApiSeccureStudent2/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿namespace ApiSeccureStudent2.Models
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    public class Student
+    {
+        [Key]
+        [Range(1, 9999)]
+        public int StudentID { get; set; }
+
+
+
+        [StringLength(30)]//lIMITA A 30 CARACTERES
+        [Required(ErrorMessage = "YOU MUST ENTER THE FIELD {0} ")]
+        public string LastName { get; set; }
+
+
+
+        [StringLength(30, ErrorMessage = "The field {0} must contain betwen {2} and {1} character ", MinimumLength = 2)]
+        [Required(ErrorMessage = "YOU MUST ENTER THE FIELD {0} ")]
+        public string FirstName { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime EnrollmentDate { get; set; }
+    }
+}
